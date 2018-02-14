f1e4ffa5b52c0c07f6fe6ee830ea963283febc6e
clambert <clambert@wifi-roaming-128-4-92-125.host.udel.edu>
2018-02-14 12:04:38 -0500

    Roll.Java Completed and Round.Java worked on

--- Lab1/src/pkgCore/Round.java
+++ Lab1/src/pkgCore/Round.java
@@ -7,12 +7,35 @@ public class Round {
 	private int ComeOutScore;
 	private eGameResult eGameResult;
 	private LinkedList<Roll> rolls = new LinkedList<Roll>();
-
+	int Score = 0;
+	
 	public Round() {
-		// TODO: Execute Come Out roll, value ComeOutScore
+		
+		Roll roll = new Roll();
+		ComeOutScore = roll.getScore();
 
-		// TODO: Create a loop that will execute a roll until point is made, or
-		// seven-out
+		if (ComeOutScore == 2 || ComeOutScore == 3 ||
+				ComeOutScore == 12) {
+			
+			//eGameResult
+		
+		}
+		
+		else if ( ComeOutScore == 7 || ComeOutScore == 11) {
+			
+			//eGameResult 
+		}
+		
+		else {
+			while (ComeOutScore != Score || ComeOutScore != 7){
+				
+				Roll newRoll = new Roll();
+				Score = newRoll.getScore();
+				ComeOutScore = Score;
+				
+			}
+			
+		}
 
 		// TODO: value the eGameResult after the round is complete
 	}
